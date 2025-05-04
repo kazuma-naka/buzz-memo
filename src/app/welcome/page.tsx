816a385f65535789cebfd73187b8a65ee8442eec
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-04 16:19:14 -0400

    make Google sign in button with shadcn

--- src/app/welcome/page.tsx
+++ src/app/welcome/page.tsx
@@ -1,13 +1,15 @@
 import Logo from '@/components/Logo';
+import GoogleSignInButton from '@/components/GoogleSignInButton';
 
 export default function WelcomePage() {
   return (
     <div className="className=min-h-screen flex flex-col bg-gray-50 text-gray-800 dark:bg-gray-900 dark:text-gray-100">
       <header
         role="banner"
-        className="flex flex-col items-center justify-center flex-1 text-center py-20"
+        className="flex flex-col items-center justify-center flex-1 text-center py-20 gap-6"
       >
         <Logo />
+        <GoogleSignInButton />
       </header>
     </div>
   );
