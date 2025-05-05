b7cdb6b4f7a580350b23bc921e39b2175fbea4ec
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-05 10:20:39 -0400

    public.users テーブルに RLS を設定する
    ユーザー自身が自分の id を取得できる
    
    Chrome の拡張機能から email から id を取得する

--- supabase/migrations/20250325225828_create_users_table.sql
+++ supabase/migrations/20250325225828_create_users_table.sql
@@ -11,3 +11,14 @@ ALTER TABLE
 CREATE POLICY "ユーザー自身が自分を更新できる" ON public.users FOR
 UPDATE
     USING (auth.uid() :: text = id);
+
+CREATE POLICY "ユーザー自身が自分を取得できる" ON public.users FOR
+SELECT
+    USING (auth.uid() :: text = id);
+
+CREATE POLICY "認証ユーザーは自分のメールアドレスでユーザー検索可" ON public.users FOR
+SELECT
+    USING (
+        auth.uid() IS NOT NULL
+        AND email = current_setting('request.jwt.claims.email')
+    );
