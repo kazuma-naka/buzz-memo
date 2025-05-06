824ff4674e440080b6a69bab4f6dd40af31ed2fd
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-06 12:38:15 -0400

    Top ページのレイアウトタグを修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,9 +11,15 @@ export default async function Home() {
   const userId = session.user?.id?.toString();
   const userEmail = session.user?.email;
   return (
-    <div>
+    <div className="bg-[#FAF9F5] min-h-screen flex flex-col">
       <Header title={'Buzz Memo'} />
-      <RegisterServiceButton userId={userId!} userEmail={userEmail!} />
+      <main className="flex-1 flex items-start justify-center pt-12 px-4">
+        <div className="w-full max-w-lg bg-white rounded-2xl shadow-lg p-8">
+          <section className="space-y-6">
+            <RegisterServiceButton userId={userId!} userEmail={userEmail!} />
+          </section>
+        </div>
+      </main>
     </div>
   );
 }
