0c56497cacf8f5547cbb66ffc1af791bb15e9df3
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-06 11:14:03 -0400

    Header をユーザーがログインしているときに Top ページで表示させる

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import { auth } from '@/auth';
 import WelcomePage from './welcome/page';
+import Header from '@/components/Header';
 
 export default async function Home() {
   const session = await auth();
   if (!session) {
     return <WelcomePage />;
   }
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
