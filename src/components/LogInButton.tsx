60a4f7e42f73d01ab9d8b566815e6b190fa93dfe
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-10 14:36:56 -0400

    redirectTo を auth/callback にしてトップページに遷移する

--- src/components/LogInButton.tsx
+++ src/components/LogInButton.tsx
@@ -23,6 +23,9 @@ export default function LogInButton() {
     const supabase = createClient();
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
+      options: {
+        redirectTo: 'http://localhost:3000/auth/callback',
+      },
     });
 
     if (error) {
