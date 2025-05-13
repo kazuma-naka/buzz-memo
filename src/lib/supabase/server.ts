79dd058089c52c26449e60c08fe59ed00f9d50af
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-12 22:40:31 -0400

    デプロイした本番環境で createClient().auth.getUser が動かないので cookie の設定を変えてみる

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,29 +1,31 @@
 import { createServerClient } from '@supabase/ssr';
 import { cookies } from 'next/headers';
 
-export async function createClient() {
+export const createClient = async () => {
   const cookieStore = await cookies();
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.NEXT_PUBLIC_SUPABASE_BACK_URL!,
+    process.env.SUPABASE_SERVICE_KEY!,
     {
+      db: {
+        schema: 'public',
+      },
+      cookieOptions: {
+        name: 'sb_token',
+      },
       cookies: {
         getAll() {
           return cookieStore.getAll();
         },
         setAll(cookiesToSet) {
           try {
-            cookiesToSet.forEach(({ name, value, options }) =>
-              cookieStore.set(name, value, options),
-            );
-          } catch {
-            // The `setAll` method was called from a Server Component.
-            // This can be ignored if you have middleware refreshing
-            // user sessions.
-          }
+            cookiesToSet.forEach(({ name, value, options }) => {
+              cookieStore.set(name, value, options);
+            });
+          } catch (error) {}
         },
       },
     },
   );
-}
+};
