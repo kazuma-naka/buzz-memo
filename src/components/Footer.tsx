3d220d6efa96c304668165bc2e2cc1d6fe36a22f
kazuma-naka <kazuma.naka.01@gmail.com>
2025-05-14 06:53:42 -0400

    GitHub のアイコンの位置の修正

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,29 @@
-`use client`;
+'use client';
 import Link from 'next/link';
+import { Github } from 'lucide-react';
 
 export default function Footer() {
   return (
     <footer className="py-6 text-center text-xl text-gray-500 bg-white">
-      &copy; {new Date().getFullYear()} Buzz Memo |{' '}
-      <Link href="/terms" className="underline">
-        利用規約
-      </Link>{' '}
-      |{' '}
-      <Link href="/privacy" className="underline">
-        プライバシーポリシー
-      </Link>{' '}
-      |{' '}
-      <Link href="/contact" className="underline">
-        お問い合わせ
-      </Link>
+      <div className="mb-4 flex justify-center items-center space-x-2">
+        <Link href="/terms" className="underline">
+          利用規約
+        </Link>
+        <span className="text-gray-400">|</span>
+        <Link href="/privacy" className="underline">
+          プライバシーポリシー
+        </Link>
+        <span className="text-gray-400">|</span>
+        <a
+          href="https://github.com/kazuma-naka/buzz-memo"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="hover:text-gray-700 transition-colors"
+        >
+          <Github size={24} />
+        </a>
+      </div>
+      <div>&copy; {new Date().getFullYear()} Buzz Memo</div>
     </footer>
   );
 }
